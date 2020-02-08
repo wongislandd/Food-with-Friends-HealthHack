c40a5e8635e65509abf9fa5b5a5fe96a52378aea
Matthew Shinder <matthewarod2@gmail.com>
2020-02-08 18:47:59 -0500

    Update CheckInActivity.java

--- app/src/main/java/com/example/foodwithfriends/CheckInActivity.java
+++ app/src/main/java/com/example/foodwithfriends/CheckInActivity.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckInActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     Switch switch1;
     //Boolean switchState;
@@ -18,27 +27,78 @@ public class CheckInActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_in);
+        mAuth = FirebaseAuth.getInstance();
 
         //switch1 = this.findViewById(R.id.switch1);
         //switchState = switch1.isChecked();
         //final MediaPlayer mp = MediaPlayer.create(this, R.raw.soho);
         final Switch switch1 = this.findViewById(R.id.switch1);
         final Switch switch2 = this.findViewById(R.id.switch2);
-        Switch switch3 = this.findViewById(R.id.switch3);
-        Switch switch4 = this.findViewById(R.id.switch4);
-        Switch switch5 = this.findViewById(R.id.switch5);
-        switch1.setOnClickListener(new View.OnClickListener(){
+        final Switch switch3 = this.findViewById(R.id.switch3);
+        final Switch switch4 = this.findViewById(R.id.switch4);
+        final Switch switch5 = this.findViewById(R.id.switch5);
 
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
-                if ((switch2.isChecked() == false) && (switch3.isChecked() == false) && (switch4.isChecked() == false) && (switch3.isChecked() == false)) {
-
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if ((switch2.isChecked() == false) && (switch3.isChecked() == false) && (switch4.isChecked() == false) && (switch5.isChecked() == false)) {
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Status", "East Side Dining");
+                    db.collection("users")
+                            .document(mAuth.getCurrentUser().getUid()).set(user
+                    );
                 }
-
-                //mp.start();
             }
         });
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if ((switch1.isChecked() == false) && (switch3.isChecked() == false) && (switch4.isChecked() == false) && (switch5.isChecked() == false)) {
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Status", "East Side Dining");
+                    db.collection("users")
+                            .document(mAuth.getCurrentUser().getUid()).set(user
+                    );
+                }
+            }
+        });
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if ((switch2.isChecked() == false) && (switch1.isChecked() == false) && (switch4.isChecked() == false) && (switch5.isChecked() == false)) {
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Status", "East Side Dining");
+                    db.collection("users")
+                            .document(mAuth.getCurrentUser().getUid()).set(user
+                    );
+                }
+            }
+        });
+        switch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if ((switch2.isChecked() == false) && (switch3.isChecked() == false) && (switch1.isChecked() == false) && (switch5.isChecked() == false)) {
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Status", "East Side Dining");
+                    db.collection("users")
+                            .document(mAuth.getCurrentUser().getUid()).set(user
+                    );
+                }
+            }
+        });
+        switch5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if ((switch2.isChecked() == false) && (switch3.isChecked() == false) && (switch4.isChecked() == false) && (switch1.isChecked() == false)) {
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Status", "East Side Dining");
+                    db.collection("users")
+                            .document(mAuth.getCurrentUser().getUid()).set(user
+                    );
+                }
+            }
+        });
+
 
 
     }
