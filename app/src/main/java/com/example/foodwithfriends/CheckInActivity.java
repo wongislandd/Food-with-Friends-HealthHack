0a7524409a6d5268578f243dae05d38a28f33e77
izan8259 <60820542+izan8259@users.noreply.github.com>
2020-02-08 12:50:32 -0500

    izan

---
+++ app/src/main/java/com/example/foodwithfriends/CheckInActivity.java
@@ -0,0 +1,15 @@
+package com.example.foodwithfriends;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CheckInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_in);
+    }
+}
+
