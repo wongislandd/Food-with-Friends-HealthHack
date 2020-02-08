b5c9074c5ef5aca92dbb78d515ba366e6fb401be
Matthew Shinder <matthewarod2@gmail.com>
2020-02-08 12:34:45 -0500

    Create ProfilePageActivity.java

---
+++ app/src/main/java/com/example/foodwithfriends/ProfilePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodwithfriends;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilePageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+    }
+}
