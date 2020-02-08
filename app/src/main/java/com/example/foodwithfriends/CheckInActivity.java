1fd2f564a7b1d3ce1cfcb6626921eae3109337f5
izan8259 <60820542+izan8259@users.noreply.github.com>
2020-02-08 13:21:41 -0500

    change1

--- app/src/main/java/com/example/foodwithfriends/CheckInActivity.java
+++ app/src/main/java/com/example/foodwithfriends/CheckInActivity.java
@@ -1,15 +1,21 @@
 package com.example.foodwithfriends;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckInActivity extends AppCompatActivity {
+
+    Switch switch1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_in);
+
+        
+
     }
 }
-
