bfaaa6d01058d9f2a8cb3a479a60d8b31329f86f
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2020-02-08 13:10:29 -0500

    Poop
    poop

--- app/src/main/java/com/example/foodwithfriends/LoginActivity.java
+++ app/src/main/java/com/example/foodwithfriends/LoginActivity.java
@@ -7,6 +7,7 @@ import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -21,6 +22,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private static final String TAG = "LoginActivity";
     @Override
     public void onStart() {
         super.onStart();
@@ -39,6 +41,31 @@ public class LoginActivity extends AppCompatActivity {
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final Button izan = findViewById((R.id.izan));
+        final Button hajime = findViewById((R.id.hajime));
+        final Button matt = findViewById((R.id.matt));
+
+        izan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               goToCheckInPage();
+            }
+        });
+        matt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToProfilePage();
+            }
+        });
+
+        hajime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: yeet");
+                goToFriendsPage();
+            }
+        });
+
 
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
@@ -89,12 +116,18 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 
+
+
     public void goToFriendsPage(){
         Intent intent = new Intent(LoginActivity.this, MyFriendsActivity.class);
         startActivity(intent);
     }
     public void goToProfilePage(){
         Intent intent = new Intent(LoginActivity.this, ProfilePageActivity.class);
+        startActivity(intent);
+    }
+    public void goToCheckInPage(){
+        Intent intent = new Intent(LoginActivity.this, CheckInActivity.class);
         startActivity(intent);
     }
 }
