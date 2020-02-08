3d5d5a2a09dc006eb97f3f5ba124c4df8a0b5915
izan8259 <60820542+izan8259@users.noreply.github.com>
2020-02-08 15:50:28 -0500

    change2

--- app/src/main/java/com/example/foodwithfriends/CheckInActivity.java
+++ app/src/main/java/com/example/foodwithfriends/CheckInActivity.java
@@ -1,6 +1,8 @@
 package com.example.foodwithfriends;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 
 import androidx.annotation.Nullable;
@@ -9,12 +11,25 @@ import androidx.appcompat.app.AppCompatActivity;
 public class CheckInActivity extends AppCompatActivity {
 
     Switch switch1;
+    //Boolean switchState;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_in);
 
+        //switch1 = this.findViewById(R.id.switch1);
+        //switchState = switch1.isChecked();
+        //final MediaPlayer mp = MediaPlayer.create(this, R.raw.soho);
+        switch1 = this.findViewById(R.id.switch1);
+        switch1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
+                //mp.start();
+            }
+        });
 
 
     }
