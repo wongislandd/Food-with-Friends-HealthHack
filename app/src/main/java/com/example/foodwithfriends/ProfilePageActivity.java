93f1e3e2dce1334eb453a31d633127ce70ffcfdf
Matthew Shinder <matthewarod2@gmail.com>
2020-02-08 12:40:50 -0500

    .
    .

--- app/src/main/java/com/example/foodwithfriends/ProfilePageActivity.java
+++ app/src/main/java/com/example/foodwithfriends/ProfilePageActivity.java
@@ -1,14 +1,25 @@
 package com.example.foodwithfriends;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class ProfilePageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_page);
+        final TextView profile_name = findViewById(R.id.profile_name);
+        final TextView profile_major = findViewById(R.id.profile_major);
+        final TextView profile_pref = findViewById(R.id.profile_pref);
+        final TextView profile_bio = findViewById(R.id.profile_bio);
+
+        profile_bio.setText("hi");
+
+
     }
 }
