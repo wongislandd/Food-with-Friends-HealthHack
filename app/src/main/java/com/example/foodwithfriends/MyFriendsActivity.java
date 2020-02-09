b1b51570ad969a7ecfda27ed034a60d73a310333
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2020-02-08 19:37:08 -0500

    Update MyFriendsActivity.java

--- app/src/main/java/com/example/foodwithfriends/MyFriendsActivity.java
+++ app/src/main/java/com/example/foodwithfriends/MyFriendsActivity.java
@@ -2,30 +2,64 @@ package com.example.foodwithfriends;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyFriendsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_friends);
-
+        getListItems();
     }
 
     private FirebaseAuth mAuth;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    
+    private static final String TAG = "MyFriendsActivity";
+    ArrayList<User> users = new ArrayList<>();
+
+    private void getListItems() {
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                users.add(new User(document.getString("Bio"),document.getString("Major"),document.getString("Name"),document.getString("Pref"),
+                                        document.getString("Status")));
+                            }
+                            initRecyclerView();
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+
 
     public void goToProfile(View view) {
         Intent intent = new Intent(getApplicationContext(),ProfilePageActivity.class);
